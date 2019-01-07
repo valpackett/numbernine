60810b0170c25f0488721ec4c64a5a4f2f0fe6cd
Greg V <greg@unrelenting.technology>
2019-01-07 15:26:38 +0300

    supervisor: implement restart loop

--- supervisor/supervisor-freebsd.cpp
+++ supervisor/supervisor-freebsd.cpp
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <poll.h>
 #include <sys/procdesc.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <iostream>
 #include <string>
@@ -58,7 +59,30 @@ void supervisor::run() {
 		data->pfds.push_back({.fd = proc->data->pd, .events = POLLHUP, .revents = 0});
 	}
 	while (true) {
-		poll(&data->pfds[0], data->pfds.size(), -1);
+		int ret = poll(&data->pfds[0], data->pfds.size(), -1);
+		if (ret < 0) {
+			std::cerr << "poll error " << errno << " " << strerror(errno) << std::endl;
+		} else if (ret > 0) {
+			for (auto it = data->pfds.begin(); it != data->pfds.end();) {
+				if (it->revents & POLLHUP) {
+					auto proc = std::find_if(data->procs.begin(), data->procs.end(), [&](auto &p) { return p->data->pd == it->fd; });
+					if (proc == data->procs.end()) {
+						std::cerr << "process with pd " << it->fd << " died, but we don't know who that is o_0 removing" << std::endl;
+						it = data->pfds.erase(it);
+						continue;
+					}
+					int status = 0;
+					auto &pr = *proc;
+					waitpid(pr->data->pid, &status, 0);
+					std::cerr << "process '" << pr->data->cmd << "' with pd " << it->fd << " died with status " << status << ", respawning in a second" << std::endl;
+					sleep(1);
+					pr->spawn();
+					it->revents = 0;
+					it->fd = pr->data->pd;
+				}
+				it++;
+			}
+		}
 	}
 }
 
