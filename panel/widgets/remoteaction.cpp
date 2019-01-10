f81cae00cb18719ec7efc66574355e8f5e0cd9aa
Greg V <greg@unrelenting.technology>
2019-01-11 01:01:20 +0300

    panel: fix remoteaction async settings access

--- panel/widgets/remoteaction.cpp
+++ panel/widgets/remoteaction.cpp
@@ -12,14 +12,15 @@ remoteaction::remoteaction(const std::string &settings_key) {
 
 	// getting from settings inside the callback guarantees live update
 	termbtn.signal_clicked().connect([this] {
+		auto action = settings->get_string("action");
 		org::gtk::Actions::createForBus(
 		    Gio::DBus::BUS_TYPE_SESSION, Gio::DBus::PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES,
 		    settings->get_string("bus-name"), settings->get_string("bus-path"),
-		    [this](Glib::RefPtr<Gio::AsyncResult> result) {
+		    [=](Glib::RefPtr<Gio::AsyncResult> result) {
 			    auto proxy = org::gtk::Actions::createForBusFinish(result);
 			    const std::vector<Glib::VariantBase> params;
 			    const std::map<Glib::ustring, Glib::VariantBase> platform_data;
-			    proxy->Activate(settings->get_string("action"), params, platform_data, [](auto _) {});
+			    proxy->Activate(action, params, platform_data, [](auto _) {});
 		    });
 	});
 
