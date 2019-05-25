dff75f7fab9dd481c1a122343cde2e3140d4470b
Greg V <greg@unrelenting.technology>
2019-05-26 01:01:30 +0300

    pk-agent: refactor into separate processes

---
+++ pk-agent/dialog.cpp
@@ -0,0 +1,162 @@
+#define POLKIT_AGENT_I_KNOW_API_IS_SUBJECT_TO_CHANGE
+#include <fmt/core.h>
+#include <gtkmm.h>
+#include <polkitagent/polkitagent.h>
+#include <sys/mman.h>
+#include <cstdlib>
+#include <cstring>
+#include "PkAgent_generated.h"
+#include "gtk-lsh/manager.hpp"
+#include "gtk-lsh/multimonitor.hpp"
+#include "gtk-lsh/surface.hpp"
+#include "gtk-util/glade.hpp"
+#define RESPREFIX "/technology/unrelenting/numbernine/pk-agent/"
+
+PolkitAgentSession *session = nullptr;
+void *auth_req_buf = nullptr;
+size_t auth_req_buf_len = 0;
+const n9::AuthRequest *auth_req = nullptr;
+
+struct dialog {
+	Glib::RefPtr<Gtk::Builder> builder = Gtk::Builder::create_from_resource(
+	    "/technology/unrelenting/numbernine/pk-agent/"
+	    "auth.glade");
+	Gtk::EventBox wrapper;
+	GLADE(Gtk::Box, toplevel);
+	GLADE(Gtk::Label, message);
+	GLADE(Gtk::Label, prompt);
+	GLADE(Gtk::Label, identity);
+	GLADE(Gtk::Entry, input);
+	GLADE(Gtk::Button, deny);
+	GLADE(Gtk::Button, allow);
+
+	std::shared_ptr<Gtk::Window> window;
+	std::optional<lsh::surface> layer_surface;
+
+	dialog(lsh::manager &lsh_mgr, Glib::ustring prompt_str, GdkMonitor *monitor) {
+		window = std::make_shared<Gtk::Window>(Gtk::WINDOW_TOPLEVEL);
+		window->set_decorated(false);
+		window->set_app_paintable(true);
+		layer_surface.emplace(lsh_mgr, window, monitor, lsh::layer::overlay);
+		(*layer_surface)
+		    ->set_anchor(lsh::anchor::top | lsh::anchor::left | lsh::anchor::bottom |
+		                 lsh::anchor::right);
+		(*layer_surface)->set_size(0, 0);
+		(*layer_surface)->set_keyboard_interactivity(1u);
+
+		auto css = Gtk::CssProvider::create();
+		css->load_from_resource(RESPREFIX "style.css");
+		window->get_style_context()->add_provider_for_screen(Gdk::Screen::get_default(), css,
+		                                                     GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+		wrapper.get_style_context()->add_class("n9-polkit-transparent-wrapper");
+		wrapper.set_above_child(false);
+
+		toplevel->set_valign(Gtk::Align::ALIGN_CENTER);
+		toplevel->set_halign(Gtk::Align::ALIGN_CENTER);
+
+		prompt->set_text(prompt_str);
+		message->set_text(auth_req->message()->str());
+		identity->set_text("TODO");
+
+		deny->signal_clicked().connect([=]() { polkit_agent_session_cancel(session); });
+
+		allow->signal_clicked().connect(sigc::mem_fun(this, &dialog::submit));
+
+		input->signal_key_release_event().connect([&](GdkEventKey *evt) {
+			if (evt->keyval == GDK_KEY_Return || evt->keyval == GDK_KEY_KP_Enter ||
+			    evt->keyval == GDK_KEY_ISO_Enter) {
+				submit();
+				return true;
+			}
+			return false;
+		});
+
+		wrapper.add(*toplevel);
+		wrapper.show_all();
+		window->add(wrapper);
+	}
+
+	void submit() {
+		auto t = input->get_text();
+		polkit_agent_session_response(session, t.c_str());
+	}
+
+	std::shared_ptr<Gtk::Window> get_window() const { return window; };
+
+	bool destroyed = false;
+
+	~dialog() {
+		if (destroyed) {
+			fmt::print(stderr, "dialog: repeat destruction!\n");
+		}
+		destroyed = true;
+		fmt::print(stderr, "dialog: destruction\n");
+		window->close();
+	}
+
+	dialog(dialog &&) = delete;
+};
+
+std::optional<lsh::manager> lsh_mgr;
+std::optional<lsh::multimonitor<dialog, lsh::manager &, Glib::ustring>> auth_dialog;
+
+void on_completed(PolkitAgentSession *, gboolean result, void *) {
+	fmt::print(stderr, "polkit: result: {}\n", result);
+	exit(69);
+}
+
+void on_request(PolkitAgentSession *, gchar *request, gboolean _echo_on, void *) {
+	fmt::print(stderr, "polkit: request: {}\n", request);
+	auth_dialog.emplace(*lsh_mgr, request);
+}
+
+void on_show_error(PolkitAgentSession *, gchar *err, void *) {
+	fmt::print(stderr, "polkit: error: {}\n", err);
+}
+
+void on_show_info(PolkitAgentSession *, gchar *inf, void *) {
+	fmt::print(stderr, "polkit: info: {}\n", inf);
+}
+
+void atexit_handler() {
+	fmt::print(stderr, "atexit: has auth_req_buf: {}\n", auth_req_buf != nullptr);
+	if (auth_req_buf) {
+		memset_s(auth_req_buf, auth_req_buf_len, 0x0, auth_req_buf_len);
+		explicit_bzero(auth_req_buf, auth_req_buf_len);
+	}
+	fmt::print(stderr, "atexit: has auth_dialog: {}\n", auth_dialog.has_value());
+}
+
+int main(int argc, char *argv[]) {
+	fmt::print(stderr, "atexit: {}\n",
+	           std::atexit(atexit_handler) == 0 ? "installed" : "not installed!!");
+
+	auto app =
+	    Gtk::Application::create(argc, argv, "technology.unrelenting.numbernine.pk-agent.dialog");
+	lsh_mgr.emplace(app);
+
+	int fd = std::stoi(Glib::getenv("N9_PK_FD"));
+	fmt::print(stderr, "auth_req: N9_PK_FD={}\n", fd);
+	{
+		struct stat recv_stat {};
+		fstat(fd, &recv_stat);
+		auth_req_buf_len = recv_stat.st_size;
+		fmt::print(stderr, "auth_req: size={}\n", auth_req_buf_len);
+	}
+	auth_req_buf = mmap(nullptr, auth_req_buf_len, PROT_READ | PROT_WRITE, MAP_PRIVATE, fd, 0);
+
+	auth_req = n9::GetAuthRequest(auth_req_buf);
+
+	auto *id = polkit_unix_user_new(getuid());
+	session = polkit_agent_session_new(id, auth_req->cookie()->c_str());
+	g_object_unref(id);
+	g_signal_connect(session, "completed", G_CALLBACK(on_completed), nullptr);
+	g_signal_connect(session, "request", G_CALLBACK(on_request), nullptr);
+	g_signal_connect(session, "show-error", G_CALLBACK(on_show_error), nullptr);
+	g_signal_connect(session, "show-info", G_CALLBACK(on_show_info), nullptr);
+	polkit_agent_session_initiate(session);
+
+	app->hold();
+	return app->run();
+}
--- pk-agent/main.cpp
+++ pk-agent/main.cpp
@@ -1,17 +1,98 @@
-#include <gtkmm.h>
+#include <fcntl.h>
+#include <fmt/format.h>
+#include <giomm.h>
+#include <signal.h>
+#include <sys/mman.h>
+#include <sys/types.h>
 #include <unistd.h>
-#include "gtk-lsh/manager.hpp"
-#include "gtk-lsh/multimonitor.hpp"
-#include "gtk-lsh/surface.hpp"
+#include "PkAgent_generated.h"
+#include "org.freedesktop.PolicyKit1.AuthenticationAgent_stub.h"
 #include "org.freedesktop.PolicyKit1.Authority_proxy.h"
-#include "polkit_agent.hpp"
+
+using Glib::ustring;
+using std::string, std::vector, std::shared_ptr, std::map, std::tuple;
+
+struct agent_impl : public org::freedesktop::PolicyKit1::AuthenticationAgentStub {
+	std::optional<MethodInvocation> auth_msg;
+	Glib::Pid dialog_pid;
+
+	void own_name() {
+		Gio::DBus::own_name(
+		    Gio::DBus::BUS_TYPE_SYSTEM, "org.freedesktop.PolicyKit1.AuthenticationAgent",
+		    [&](const Glib::RefPtr<Gio::DBus::Connection>& connection, const ustring& name) {
+			    fmt::print(stderr, "dbus: name: {}\n", name.c_str());
+			    if (register_object(connection, "/org/freedesktop/PolicyKit1/AuthenticationAgent") == 0) {
+				    fmt::print(stderr, "dbus: failed to register object\n");
+				    exit(1);
+			    }
+		    },
+		    Gio::DBus::SlotNameAcquired(),
+		    [&](const Glib::RefPtr<Gio::DBus::Connection>& /* connection */, const ustring& name) {
+			    fmt::print(stderr, "dbus: name '{}' lost (supposedly)\n", name.c_str());
+			    // we get this even though everything is fine??
+		    });
+	}
+
+	void on_dialog_exit(Glib::Pid pid, int code) {
+		fmt::print(stderr, "child: exit {} {}\n", pid, code);
+		auth_msg->ret();
+	}
+
+	void BeginAuthentication(
+	    const ustring& action_id, const ustring& message, const ustring& icon_name,
+	    const map<ustring, ustring>& details, const ustring& cookie,
+	    const vector<tuple<ustring, map<ustring, Glib::VariantBase>>>& identities,
+	    MethodInvocation& msg) override {
+		using namespace n9;
+		using namespace flatbuffers;
+		fmt::print(stderr, "dbus: action='{}' msg='{}' icon='{}'\n ", string(action_id),
+		           string(message), string(icon_name));
+		auth_msg = msg;
+		FlatBufferBuilder builder(8192);
+		vector<uint8_t> fident_types;
+		vector<Offset<void>> fidents;
+		for (const auto& [typ, params] : identities) {
+			if (typ != "unix-user") {
+				fmt::print(stderr, "dbus: XXX: authenticating as {} instead of unix-user??", typ.c_str());
+				fident_types.push_back(static_cast<uint8_t>(Ident::UnknownIdent));
+				fidents.push_back(CreateUnknownIdent(builder, builder.CreateString(typ)).Union());
+				continue;
+			}
+			fident_types.push_back(static_cast<uint8_t>(Ident::UnixUserIdent));
+			fidents.push_back(
+			    CreateUnixUserIdent(builder, builder.CreateString(params.at("uid").print())).Union());
+		}
+		builder.Finish(
+		    CreateAuthRequest(builder, builder.CreateString(action_id), builder.CreateString(cookie),
+		                      builder.CreateString(message), builder.CreateString(icon_name),
+		                      builder.CreateVector(fident_types), builder.CreateVector(fidents)));
+		int fd = shm_open(SHM_ANON, O_RDWR | O_CREAT, 0644);
+		ftruncate(fd, builder.GetSize());
+		write(fd, builder.GetBufferPointer(), builder.GetSize());
+		lseek(fd, 0, SEEK_SET);
+		vector<string> argv{
+		    "/home/greg/src/github.com/myfreeweb/numbernine/build/pk-agent/n9-pk-agent-dialog"};
+		Glib::setenv("N9_PK_FD", fmt::format("{}", fd));
+		fcntl(fd, F_SETFD, 0);
+		Glib::spawn_async("", argv, Glib::SPAWN_LEAVE_DESCRIPTORS_OPEN | Glib::SPAWN_DO_NOT_REAP_CHILD,
+		                  Glib::SlotSpawnChildSetup(), &dialog_pid);
+		fmt::print(stderr, "child: spawn {}\n", dialog_pid);
+		Glib::signal_child_watch().connect(sigc::mem_fun(*this, &agent_impl::on_dialog_exit),
+		                                   dialog_pid);
+		close(fd);
+	}
+
+	void CancelAuthentication(const ustring& cookie, MethodInvocation& msg) override {
+		kill(dialog_pid, SIGTERM);
+	}
+};
 
 int main(int argc, char* argv[]) {
-	auto app = Gtk::Application::create(argc, argv, "technology.unrelenting.numbernine.pk-agent");
-	lsh::manager lsh_mgr(app);
-	polkit_agent agent(lsh_mgr);
+	Gio::init();
+
+	agent_impl agent;
 	agent.own_name();
-	app->hold();
+
 	Glib::signal_idle().connect([&] {
 		org::freedesktop::PolicyKit1::AuthorityProxy::createForBus(
 		    Gio::DBus::BUS_TYPE_SYSTEM, Gio::DBus::PROXY_FLAGS_DO_NOT_LOAD_PROPERTIES,
@@ -20,17 +101,19 @@ int main(int argc, char* argv[]) {
 			    const auto proxy =
 			        org::freedesktop::PolicyKit1::AuthorityProxy::createForBusFinish(result);
 			    // TODO find current session
-			    const Glib::Variant<Glib::ustring> var =
-			        Glib::Variant<Glib::ustring>::create("/org/freedesktop/ConsoleKit/Session1");
+			    const Glib::Variant<ustring> var =
+			        Glib::Variant<ustring>::create("/org/freedesktop/ConsoleKit/Session1");
 			    proxy->RegisterAuthenticationAgent({"unix-session", {{"session-id", var}}}, "en_US.UTF-8",
 			                                       "/org/freedesktop/PolicyKit1/AuthenticationAgent",
 			                                       [=](auto resp) {
 				                                       proxy->RegisterAuthenticationAgent_finish(resp);
-				                                       std::cerr << "reg" << std::endl;
+				                                       fmt::print(stderr, "dbus: registered at polkit\n");
 			                                       });
 		    });
 		return false;
 	});
 
-	return app->run();
+	auto loop = Glib::MainLoop::create();
+	loop->run();
+	return 0;
 }
